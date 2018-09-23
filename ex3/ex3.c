0656cc0b8d012b7b103f3422f8ed6bbdd7380a05
Roger Molas <r.molas06@gmail.com>
2018-09-23 22:02:45 +0800

    Finished ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-struct {
-  int age;
-  char name[];
-  
-}p;
-
 int main()
 {
   int age = 10;
   int height = 72;
+
+  char name[100];
   
   printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
 
-  
-  
+  printf("Enter your name: ");
+  scanf("%s", name);
+
+  printf( "\nYou entered: %s ", name);  
+ 
   return 0;
 }
